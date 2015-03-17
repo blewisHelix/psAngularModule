4418fb2798971a850961fbbca622099cc795c2a2
Brad Lewis <blewis@helixeducation.com>
2015-03-17 14:35:33 -0600

    finished with module 3

--- scripts.js
+++ scripts.js
@@ -6,14 +6,27 @@
 
 		var onUserComplete = function(response){
 			$scope.user = response.data;
+			$http.get($scope.user.repos_url)
+				.then(onRepos, onError);
+		};
+		
+		var onRepos = function(response){
+			$scope.repos = response.data;
 		};
 
 		var onError = function(reason){
-			$scope.error = "could not fetch the user";
+			$scope.error = "could not fetch the data";
 		};
 		
-		$http.get("https://api.github.com/users/blewisHelix")
-			.then(onUserComplete,onError);
+		$scope.search = function(username){
+			$http.get("https://api.github.com/users/" + username)
+				.then(onUserComplete,onError);
+		};
+
+		$scope.username = "angular";
+		$scope.message = "Github Viewer";
+		$scope.repoSortOrder = "-stargazers_count";
+
 	}
 
 	app.controller("MainController", ["$scope", "$http", MainController]);
